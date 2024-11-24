d21ae207076f6f5afb5ac8bbad63a676e7fb5612
GomezDario <dariogomez.g00@gmail.com>
2024-11-24 20:41:27 -0300

    integrando mapa

--- paginaEpers/src/components/MapaEspiritus.jsx
+++ paginaEpers/src/components/MapaEspiritus.jsx
@@ -1,11 +1,41 @@
 import React from "react";
-import "./MapaEspiritus.css"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css"; // Importa los estilos de Leaflet
+import L from "leaflet";
+import "./MapaEspiritus.css";
+
+// Corrige el problema con los íconos de Leaflet
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const MapWorld = () => {
   return (
     <div className="container">
       {/* Contenedor para el mapa */}
-      <div id="map"></div>
+      <MapContainer
+        id="map"
+        center={[-34.6037, -58.3816]} // Coordenadas de Buenos Aires
+        zoom={13} // Nivel de zoom
+        style={{ height: "400px", width: "100%" }} // Ajusta el tamaño del mapa
+      >
+        
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        
+        <Marker position={[-34.6037, -58.3816]}>
+          <Popup>
+            Buenos Aires, Argentina <br /> ¡Aquí está el marcador!
+          </Popup>
+        </Marker>
+      </MapContainer>
+
       <div className="section">
         {/* Sección de mediums */}
         <div className="mediums">
@@ -27,7 +57,7 @@ const MapWorld = () => {
           <button>Crear</button>
         </div>
 
-        {/* Sección de espíritus */}
+        
         <div className="spirits">
           <h3>ESPÍRITUS</h3>
           <div className="form-group">
